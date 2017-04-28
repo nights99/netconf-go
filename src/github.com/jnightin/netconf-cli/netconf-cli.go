fde614563be8d059278448632cd058dc378568b9
Jonathan Nightingale -X (jnightin - ENSOFT LIMITED at Cisco) <jnightin@cisco.com>
2017-04-28 15:55:10 +0100

    Add readline

--- src/github.com/jnightin/netconf-cli/netconf-cli.go
+++ src/github.com/jnightin/netconf-cli/netconf-cli.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 
+	"github.com/chzyer/readline"
 	"github.com/openconfig/goyang/pkg/yang"
 )
+
+var completer = readline.NewPrefixCompleter(readline.PcItem("get"))
 
 func main() {
 
@@ -42,6 +49,39 @@ func main() {
 		for s1, e1 := range e.Dir {
 			println(s1)
 			e1.Print(os.Stdout)
+		}
+	}
+
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:            "\033[31m»\033[0m ",
+		HistoryFile:       "/tmp/readline.tmp",
+		AutoComplete:      completer,
+		InterruptPrompt:   "^C",
+		EOFPrompt:         "exit",
+		HistorySearchFold: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	log.SetOutput(l.Stderr())
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+
+		line = strings.TrimSpace(line)
+		switch {
+		case strings.HasPrefix(line, "get "):
+		default:
+			log.Println("you said:", strconv.Quote(line))
 		}
 	}
 }
