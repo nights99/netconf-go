3d6cecd171cdf4f14f96f3a3d003595fe1e03167
Jonathan Nightingale -X (jnightin - ENSOFT LIMITED at Cisco) <jnightin@cisco.com>
2017-04-28 16:23:29 +0100

    Add yang completion

--- src/github.com/jnightin/netconf-cli/netconf-cli.go
+++ src/github.com/jnightin/netconf-cli/netconf-cli.go
@@ -13,7 +13,21 @@ import (
 	"github.com/openconfig/goyang/pkg/yang"
 )
 
-var completer = readline.NewPrefixCompleter(readline.PcItem("get"))
+var completer = readline.NewPrefixCompleter(readline.PcItem("get", readline.PcItemDynamic(listYang("./"))))
+
+func listYang(path string) func(string) []string {
+	return func(line string) []string {
+		names := make([]string, 0)
+		/*files, _ := ioutil.ReadDir(path)
+		for _, f := range files {
+			names = append(names, f.Name())
+		}
+		return names*/
+		println(path)
+		names = append(names, "hostname")
+		return names
+	}
+}
 
 func main() {
 
@@ -52,6 +66,8 @@ func main() {
 		}
 	}
 
+	fmt.Printf("\n\n\n\n")
+
 	l, err := readline.NewEx(&readline.Config{
 		Prompt:            "\033[31m»\033[0m ",
 		HistoryFile:       "/tmp/readline.tmp",
@@ -61,27 +77,38 @@ func main() {
 		HistorySearchFold: true,
 	})
 	if err != nil {
+		println("Error!")
 		panic(err)
 	}
 	defer l.Close()
 	log.SetOutput(l.Stderr())
 	for {
+		println("In loop")
 		line, err := l.Readline()
 		if err == readline.ErrInterrupt {
 			if len(line) == 0 {
+				println("Error!")
+				println("Error!")
+				println("Error!")
+				println("Error!")
+				println("Error!")
 				break
 			} else {
 				continue
 			}
 		} else if err == io.EOF {
+			println("Error!")
+			println("Error!")
+			println("Error!")
 			break
 		}
 
 		line = strings.TrimSpace(line)
 		switch {
-		case strings.HasPrefix(line, "get "):
+		case strings.HasPrefix(line, "get"):
 		default:
 			log.Println("you said:", strconv.Quote(line))
 		}
 	}
+
 }
